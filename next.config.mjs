2112f11a1f5d88d46b02ebd994da825e947bef49
Mahathir-Mohammod <mahathirmohammod88@gmail.com>
2026-05-21 20:28:21 +0600

    fix: hardcode server URL in next.config rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/:path*`,
+        destination: "https://ideavault-server-gamma.vercel.app/api/auth/:path*",
       },
     ];
   },
